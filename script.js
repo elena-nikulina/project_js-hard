3c47569420820e5984a088db975a9a1a1459ad8b
elena-nikulina <info@webstar-support.com>
2022-03-26 00:04:35 +0700

    Исправлен вывод произведения

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ for(let i = 0; i < (num.toString()).length; i++) {
     num1 = +(((num.toString())).split(''))[i]*num1;
 }
 
-
+console.log(num1);
 console.log(((num1**3).toString()).substring(0, 2));
