40c9d7c1862fb3d068eb7857ba41515b667bb0ce
elena-nikulina <info@webstar-support.com>
2022-03-25 22:32:54 +0700

    Сделала правки к усложненному уроку

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ let arr = str.split('');
 let num1 = 1;
 
 
-for(let i = 0; i < arr.length; i++) {
-    num1 = +arr[i]*num1;
-    console.log(num1);
+for(let i = 0; i < (num.toString()).length; i++) {
+    num1 = +(((num.toString())).split(''))[i]*num1;
 }
 
-let num2 = num1**3;
-let str1 = num2.toString();
-let str2 = str1.substring(0, 2);
 
-console.log(str2);
+console.log(((num1**3).toString()).substring(0, 2));
