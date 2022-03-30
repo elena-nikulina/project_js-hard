c4c0c25d4b108cc4561e061f616d9a647d2a14b0
elena-nikulina <info@webstar-support.com>
2022-03-30 13:48:25 +0700

    Сделала сложное задание к уроку 3

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-let num = 266219;
-let num1 = 1;
+let lang = 'ru';
 
+let days_ru = [
+    'вс', 'пн', 'вт', 'ср', 'чт', 'пн', 'сб'
+];
 
-for(let i = 0; i < (num.toString()).length; i++) {
-    num1 = +(((num.toString())).split(''))[i]*num1;
+let days_en = [
+    'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'
+];
+
+let date = new Date();
+let day_number = date.getDay();
+
+if (lang = 'ru') {
+    console.log(days_ru[day_number]);
+} else if (lang = 'en') {
+    console.log(days_en[day_number]);
 }
 
-console.log(num1);
-console.log(((num1**3).toString()).substring(0, 2));
+switch(lang) {
+    case 'ru': 
+        console.log(days_ru[day_number]);
+        break;
+    case 'en':
+        console.log(days_en[day_number]);
+        break;
+    default:
+        console.log('Такого языка не предусмотрено!');
+        break;
+}
+
+let lang_arr = [ ['вс', 'пн', 'вт', 'ср', 'чт', 'пн', 'сб'], 
+                ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']];
+
+let r = 0;
+let p = 0;
+console.log(lang_arr[0][day_number]);
+while ((lang = 'ru') && (r = 0)) {
+    console.log(lang_arr[0][day_number]);
+    r++;
+}
+    
+while ((lang = 'en') && (p = 0)) {
+    console.log(lang_arr[1][day_number]);
+    p++;
+}
+ 
+let namePerson = 'Артем';
+let namePerson1 = namePerson=='Артем' ? 'директор' : 'студент';
+let namePerson2 = namePerson=='Александр' ? 'преподаватель' : 'студент';
+
+console.log(namePerson1);
+console.log(namePerson2);
