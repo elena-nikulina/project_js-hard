8e3ae09e86c317447a8811822bc6e3f64315eb87
elena-nikulina <info@webstar-support.com>
2022-05-07 13:01:56 +0700

    Сделала усложненку к уроку 7

--- script.js
+++ script.js
@@ -19,28 +19,25 @@ if (isNumber(num1)) {
     console.log(num1, typeof(num1));
 }*/
 
-let arr=[];
-let arr1 = [];
+const week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
 
-arr = ['2435', '342189', '4312670', '13256432', '354', '42318', '897642'];
+let today = new Date();
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] === '2' || arr[i][0] === '4') {
-        console.log(arr[i]);
+let dayToday = today.getDay();
+
+for(let day of week) {
+    if (day !== dayToday && day !== week[0] && week[6]) {
+        console.log(day);
     }
+    if (day === week[0] || day === week[6]) {
+        //day.style.fontStyle = 'italic';
+        console.log('<i>' + day + '</i>');
+    }
+    if (day === dayToday) {
+        //day.style.fontStyle = 'bold';
+        console.log('<strong>' + day + '</strong>');
+    }
+
 }
 
-console.log(1 + ' Делители: ' + ' 1 ');
-console.log(2 + ' Делители: ' + ' 1 ' + 2);
-console.log(3 + ' Делители: ' + ' 1 ' + 3);
-console.log(5 + ' Делители: ' + ' 1 ' + 5);
-console.log(7 + ' Делители: ' + ' 1 ' + 7);
-
-for (let j = 8; j < 100; j++) {
-    
-    if (j % 2 !== 0 && j % 3 !== 0 && j % 5 !== 0 && j % 7 !== 0) {
-        arr1.push(j);
-        console.log(j + ' Делители: ' + ' 1 ' + j);
-    }
-}
 
